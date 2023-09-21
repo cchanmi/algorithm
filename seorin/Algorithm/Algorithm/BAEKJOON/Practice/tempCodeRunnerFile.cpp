b71e7e5c1d449f7017e1b12c99e08db965101cc8
유서린 <sprout3082@gmail.com>
2023-09-21 19:24:22 +0900

    vscode 설정

---
+++ seorin/Algorithm/Algorithm/BAEKJOON/Practice/tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+for (int i=1; i<=n; i++) {
+    //     for (long  j=1; j<=m; j++) {
+    //         if (mem[i]<=j) {
+    //             price[i][j] = min(price[i-1][j], cost[i]);
+    //         } else {
+    //             cout << price[i-1][j-mem[i]] << " ";
+    //             price[i][j] = min(price[i-1][j], price[i-1][j-mem[i]] + cost[i]);
+    //         }
+    //     }
+    //     cout << "\n";
+    // }

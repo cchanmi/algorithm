dce5aa902a846a8248935950b49712d722257d1c
cchanmi <113524@naver.com>
2024-08-20 13:31:49 +0900

    leetcode - 344

---
+++ python/string/leetcode_344.py
@@ -0,0 +1,20 @@
+# LeetCode
+# 344. Reverse String
+# https://leetcode.com/problems/reverse-string/
+
+def mySolution(s: [str]) -> None:
+    # 그냥 뒤집는 방법
+    s.reverse()
+    
+    # 공간복잡도가 O(1)이기 때문에 해당 상황을 고려한 방벙
+    s[:] = s[::-1]
+        
+def twoPointersSolution(s: [str]) -> None:
+    # 투 포인터 사용
+    left = 0
+    right = len(s)-1
+
+    while left < right:
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
